1d7c6f2e833891070b1e38485e42d64beb9ef426
unknown <mabdullah0043@stu.ksa.edu.sa>
2022-01-25 00:46:39 +0300

    update

---
+++ resources/views/invoices/index.blade.php
@@ -0,0 +1,61 @@
+@extends('products.layout')
+ 
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Products</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+    <div class="card-body">
+            <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <input type="file" name="file" class="form-control">
+                <br>
+                <button class="btn btn-success">Import User Data</button>
+                <a class="btn btn-warning" href="{{ route('export') }}">Export User Data</a>
+            </form>
+        </div>
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->price }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $products->links() !!}
+      
+@endsection
